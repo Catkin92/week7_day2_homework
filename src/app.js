b5d3d99755aca63a8fd4d053ad140a59ceb725de
Catrin <catrinhughes92@gmail.com>
2019-11-26 17:19:44 +0000

    exchange euros either way

--- src/app.js
+++ src/app.js
@@ -4,7 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue ({
     el: "#app",
     data: {
-      exchangeRates: null
+      exchangeRates: null,
+      selectedRate: null,
+      selectedRate2: null,
+      inputtedAmount: null,
+      inputtedAmount2: null,
+      exchanged: null
     },
     mounted(){
       this.exchangeRate();
@@ -14,6 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
         const request = fetch("https://api.exchangeratesapi.io/latest")
         .then(response => response.json())
         .then(info => this.exchangeRates = info.rates)
+      },
+      euroExchange: function() {
+        const money = (this.inputtedAmount) * (this.selectedRate);
+        this.exchanged = money.toFixed(2);
+
+      },
+      otherExchange: function() {
+        const money = ((this.inputtedAmount2) / (this.selectedRate2));
+        this.exchanged = `€${money.toFixed(2)}`;
       }
     }
 
